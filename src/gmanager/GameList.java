acda40ba95933d3d98fc3577df231da6888d5bcf
david <zoidberg@zoidberg-3000-N200.(none)>
2013-06-11 09:47:46 +0200

    added a gui object for the gameexplorer
    i also reverted some changes in the collapseable list

---
+++ src/gmanager/GameList.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gmanager;
+
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author zoidberg
+ */
+public class GameList extends JPanel implements ActionListener {
+    private Game game;
+    private JPanel gameList;
+    private JLabel header;
+    private JTextField SearchField;
+    private JButton Search;
+    
+    public GameList (){
+        
+        
+        
+        
+    }
+    
+    
+}
