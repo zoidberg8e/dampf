f22e6165c20a94cab16aa8dfbdeac42f9fa2c78f
david <zoidberg@zoidberg-3000-N200.(none)>
2013-06-12 12:57:58 +0200

    Added some features to the GAMELIST
    8======================D
    montag pizza essen und so

--- src/gmanager/GameList.java
+++ src/gmanager/GameList.java
@@ -4,11 +4,14 @@
  */
 package gmanager;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 /**
@@ -17,22 +20,29 @@ import javax.swing.JTextField;
  */
 public class GameList extends JPanel implements ActionListener {
     private Game game;
-    private JPanel gameList;
+    private JScrollPane gameList;
     private JLabel header;
     private JTextField SearchField;
     private JButton Search;
     
     public GameList (){
-        
+        super();
+        setLayout(new BorderLayout(4,5));
+        JPanel Searchbar = new JPanel();
+        Searchbar.setLayout(new FlowLayout(FlowLayout.LEFT));
+        add(Searchbar,BorderLayout.PAGE_START);
+        SearchField = new JTextField(100);
+        Search = new JButton("Search");
+        Searchbar.add(SearchField);
+        Search.addActionListener(this);
+        gameList = new JScrollPane();
         
         
         
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void actionPerformed(ActionEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    
 }
